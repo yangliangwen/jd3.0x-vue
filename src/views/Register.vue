9d738b4fafebf9aedfd18245f300afaed4a46437
yangliangwen <1135805457@qq.com>
2020-12-11 20:48:31 +0800

    格式修改

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
     <cube-form :model="model" :schema="schema" @submit="submitHandler"></cube-form>
-    账号 {{model.username}} 密码{{model.password}}
+    账号 {{ model.username }} 密码{{ model.password }}
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
       model: {
         username: "",
-        password: ""
+        password: "",
       },
       schema: {
         fields: [
@@ -20,56 +19,56 @@ export default {
             modelKey: "username",
             label: "账号",
             props: {
-              placeholder: "请输入账号"
+              placeholder: "请输入账号",
             },
             rules: {
-              required: true
+              required: true,
             },
             // validating when blur
-            trigger: "blur"
+            trigger: "blur",
           },
           {
             type: "input",
             modelKey: "password",
             label: "密码",
             props: {
-              type:'password',
-              placeholder: "请输入密码"
+              type: "password",
+              placeholder: "请输入密码",
             },
             rules: {
-              required: true
+              required: true,
             },
             // validating when blur
-            trigger: "blur"
+            trigger: "blur",
           },
           {
             type: "submit",
-            label: "注册"
-          }
-        ]
+            label: "注册",
+          },
+        ],
       },
       options: {
         scrollToInvalidField: true,
-        layout: "standard" // classic fresh
-      }
+        layout: "standard", // classic fresh
+      },
     };
   },
   methods: {
     submitHandler(e) {
       e.preventDefault();
       // console.log("submit", this.model);
-      this.$http.get("/api/register", { params: this.model }).then(res => {
+      this.$http.get("/api/register", { params: this.model }).then((res) => {
         console.log(res);
         if (res.data.success) {
           console.log("注册页面传值", this.model);
           this.$router.push({
             name: "login",
-            params: this.model
+            params: this.model,
           });
         }
       });
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="stylus" scoped></style>
